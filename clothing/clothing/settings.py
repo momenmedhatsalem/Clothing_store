a61f9567b5725c3f6db2fc87a73c72979506be82
unknown <momenmedhatsalem@gmail.com>
2023-06-24 09:27:04 +0300

    Fixed Template js sync

--- clothing/clothing/settings.py
+++ clothing/clothing/settings.py
@@ -125,9 +125,10 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 # Application definition
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend/static"),
     
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SITE_ID = 1
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
