0ffc13746a4dbe4435448d421094e6a5586253ed
unknown <momenmedhatsalem@gmail.com>
2023-06-29 17:48:32 +0300

    added os

--- clothing/clothing/settings.py
+++ clothing/clothing/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import sys
-
+import os
 import dj_database_url
 
 from pathlib import Path
