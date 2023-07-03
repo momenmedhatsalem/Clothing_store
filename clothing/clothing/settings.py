eb2bab9b587791b6bc17411a8157d062b329069c
unknown <momenmedhatsalem@gmail.com>
2023-07-03 20:43:24 +0300

    Added gitignore

--- clothing/clothing/settings.py
+++ clothing/clothing/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
