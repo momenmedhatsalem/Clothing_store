64526c6ee966da5f2b7e112cb68004de08e1e5c3
unknown <momenmedhatsalem@gmail.com>
2023-07-04 06:29:50 +0300

    Added whitenoise

--- clothing/clothing/settings.py
+++ clothing/clothing/settings.py
@@ -145,13 +145,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-   
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 SITE_ID = 1
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
