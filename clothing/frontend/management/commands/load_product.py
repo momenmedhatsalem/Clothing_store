94b5430bb55eb8c53fff6d0e630299cd12c2ff50
unknown <momenmedhatsalem@gmail.com>
2023-07-04 10:46:21 +0300

    Added Products with photos

--- clothing/frontend/management/commands/load_product.py
+++ clothing/frontend/management/commands/load_product.py
@@ -22,28 +22,35 @@ class Command(BaseCommand):
 
 
 
-        #Code to load the data into database
         for row in DictReader(open('./Products.csv')):
-            product=Product(product_name=row['product_name'], category=row['category'], label=row['label'], subcategory=['subcategory'], price=row['price'], discount_price=row['discount_price'], pub_date=row['pub_date'])  
-            product.save()
+            # Check if product with this id already exists
+            product_id = row['id']
+            existing_product = Product.objects.filter(id=product_id).first()
+            if existing_product:
+                # Product already exists, skip creating a new one
+                continue
+            else:
+                # Product does not exist, create a new one
+                product = Product(product_name=row['product_name'], category=row['category'], label=row['label'], subcategory=['subcategory'], price=row['price'], discount_price=row['discount_price'], pub_date=row['pub_date'])
+                product.save()
 
-            # Get size data for this product from the file
-            sizes = row['sizes'].split(';')
-            for size in sizes:
-                size_data = size.split(':')
-                size_name = size_data[0]
-                size_quantity = int(size_data[1])
-                size_instance = ProductSize(product=product, size=size_name, quantity=size_quantity)
-                size_instance.save()
+                # Get size data for this product from the file
+                sizes = row['sizes'].split(';')
+                for size in sizes:
+                    size_data = size.split(':')
+                    size_name = size_data[0]
+                    size_quantity = int(size_data[1])
+                    size_instance = ProductSize(product=product, size=size_name, quantity=size_quantity)
+                    size_instance.save()
 
-            # Get color data for this product from the file
-            colors = row['colors'].split(';')
-            for color in colors:
-                color_data = color.split(':')
-                color_name = color_data[0]
-                color_id = color_data[1]
-                color_quantity = int(color_data[2])
-                color_instance = ProductColor(product=product, color=color_name, color_id=color_id, quantity=color_quantity)
-                color_instance.save()
+                # Get color data for this product from the file
+                colors = row['colors'].split(';')
+                for color in colors:
+                    color_data = color.split(':')
+                    color_name = color_data[0]
+                    color_id = color_data[1]
+                    color_quantity = int(color_data[2])
+                    color_instance = ProductColor(product=product, color=color_name, color_id=color_id, quantity=color_quantity)
+                    color_instance.save()
 
 
