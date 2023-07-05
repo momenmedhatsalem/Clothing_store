395ccf8fed30a79ed4a08b8df52236bd635874d9
unknown <momenmedhatsalem@gmail.com>
2023-07-05 07:50:56 +0300

    Added sitemaps

--- clothing/clothing/urls.py
+++ clothing/clothing/urls.py
@@ -18,9 +18,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.sitemaps.views import sitemap
+
+from frontend.sitemaps import ProductSitemap
+
+sitemaps = {
+		"Products": ProductSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('frontend.urls')),
     path('accounts/', include('allauth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+                name='django.contrib.sitemaps.views.sitemap')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
