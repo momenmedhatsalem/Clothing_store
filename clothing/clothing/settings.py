208dec93aa00703546d56cd0989027d6253e273e
unknown <momenmedhatsalem@gmail.com>
2023-07-05 08:39:07 +0300

    debug

--- clothing/clothing/settings.py
+++ clothing/clothing/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "False"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["www.vos-mos.com", "vos-mos.com"]
 
