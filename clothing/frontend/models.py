2941b63581ea244f5b9045d28affda2d7382cd89
unknown <momenmedhatsalem@gmail.com>
2023-07-20 18:41:09 +0300

    fixed prdct load

--- clothing/frontend/models.py
+++ clothing/frontend/models.py
@@ -52,7 +52,7 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to=rename_image, default="", blank=True, null=True)
     path = models.CharField(max_length=50, default="")   
-
+    color = models.CharField(max_length=50, default="")
     def __str__(self):
         return self.product.product_name
     
